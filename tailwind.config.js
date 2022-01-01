e69bf95c9761325f678d2bbb6335c80f5733a543
shareef <nadeemshareef934@gmail.com>
2022-01-01 13:01:44 +0530

    Add project

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    content: ["./index.html"],
+    theme: {
+        container: {
+            center: true,
+            padding: "1rem",
+        },
+        extend: {
+            colors: {
+                darkGreen: "var(--dark-green)",
+                lightGreen: "var(--light-green)",
+                green: "var(--green)",
+            },
+        },
+    },
+    plugins: [],
+};
